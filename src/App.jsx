1f62ee53330fc28ba405ff69876959f1d513eb27
Artur Przystaś <artek.przystas@gmail.com>
2022-10-05 23:52:00 +0200

    setting the thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
+
+//compontents
+import Start from './compontents/Start';
+
+//styles
 import './App.css'
 
 function App() {
-  
+  const [mode, setMode] = useState("start");
+  const [chosenThing, setChosenThing] = useState("");
+
+  const handleThingChoose = value => {
+    setChosenThing(thing);
+  }
 
   return (
     <div>
-      
+      {mode==='start' && <Start setChosenThing={setChosenThing} />}
     </div>
   )
 }
